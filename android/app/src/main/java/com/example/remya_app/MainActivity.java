0b22eda10e7d31b3a8de3d4009354b50dd0f4dec
Remya <remyaprabhakaran93@gmail.com>
2022-02-22 22:08:30 +0530

    initial commit

---
+++ android/app/src/main/java/com/example/remya_app/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.remya_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+
+
+
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+        title:const Text("My Project"),
+        leading: const Icon(Icons.arrow_back_rounded),
+        actions: const [
+          Icon(Icons.search),
+          Icon(Icons.admin_panel_settings_rounded),
+        ],
+        backgroundColor: Colors.green,
+        elevation: 0,
+      ),
+
+    );
+  }
+}
